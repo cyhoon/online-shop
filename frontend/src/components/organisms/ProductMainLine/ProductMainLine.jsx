a93d8ca63d81016409a2857fa2a677c3988399e7
JeffChoi <ihello0720@gmail.com>
2018-06-18 15:52:05 +0900

    Backend 수정 및 category 페이지 개발 중

--- frontend/src/components/organisms/ProductMainLine/ProductMainLine.jsx
+++ frontend/src/components/organisms/ProductMainLine/ProductMainLine.jsx
@@ -3,10 +3,33 @@ import styled from 'styled-components';
 import Title from '../../atoms/Title';
 
 const ProductMainLine = ({ }) => {
+    const ContentContainer = styled.div`
+        display: flex;
+    `;
+
+    const UlContainer = styled.ul`
+        flex: 1;
+    `;
+
     return (
         <div>
             <Title text="인기상품" textAlign="center" />
-            ProductMainLine
+            <ContentContainer>
+                <UlContainer>
+                    <li>1</li>
+                    <li>1</li>
+                    <li>1</li>
+                    <li>1</li>
+                    <li>1</li>
+                </UlContainer>
+                <UlContainer>
+                    <li>2</li>
+                    <li>2</li>
+                    <li>2</li>
+                    <li>2</li>
+                    <li>2</li>
+                </UlContainer>
+            </ContentContainer>
         </div>
     );
 };
