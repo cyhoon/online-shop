d520d2ccadcfff859eff765e58e90e35da567879
JeffChoi <ihello0720@gmail.com>
2018-06-19 23:20:04 +0900

    login redux 및 서버 연결 시작

--- frontend/src/containers/LoginContainer.jsx
+++ frontend/src/containers/LoginContainer.jsx
@@ -5,12 +5,35 @@ import LoginMain from '../components/organisms/LoginMain';
 class LoginContainer extends Component {
     constructor(props) {
         super(props);
+
+        this._login = (e) => {
+            // login logic
+        };
+    
+        this._onChangeUserId = (e) => {
+            this.setState({
+                user_id: e.target.value,
+            });
+        };
+    
+        this._onChangeUserPw = (e) => {
+            this.setState({
+                user_pw: e.target.value,
+            });
+        };
+
+        this.state = {
+            'user_id': null,
+            'user_pw': null,
+        };
     }
 
     render() {
         return (
             <div>
-                <LoginMain />
+                <LoginMain
+                    
+                />
             </div>
         );
     }
